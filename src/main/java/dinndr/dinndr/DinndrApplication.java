f0e523d3abd0ea7fbfc8dc17ea0c8c0182c7ebae
Christopher A Megown <megown1204@gmail.com>
2017-12-06 17:46:57 -0500

    first commit

---
+++ src/main/java/dinndr/dinndr/DinndrApplication.java
@@ -0,0 +1,12 @@
+package dinndr.dinndr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DinndrApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DinndrApplication.class, args);
+	}
+}
---
+++ src/test/java/dinndr/dinndr/DinndrApplicationTests.java
@@ -0,0 +1,16 @@
+package dinndr.dinndr;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class DinndrApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
