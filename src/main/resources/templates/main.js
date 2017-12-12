c2defa669fd4420b3dc0c6f9eb78c13ab1c6502b
Michael Smith <mjsmitths@gmail.com>
2017-12-11 19:45:57 -0500

    commit

---
+++ src/main/resources/templates/main.js
@@ -0,0 +1,7 @@
+function viewMore() {
+	document.getElementById("viewMoreButton").addEventListener("click", function(){
+		  document.getElementById("information").innerHTML = "To order visit their website.";
+		});
+}
+
+
