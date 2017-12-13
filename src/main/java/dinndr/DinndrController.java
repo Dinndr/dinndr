0f27ca59df58cb6a5f3cdddb76e6d1eacd60ac4b
Christopher A Megown <megown1204@gmail.com>
2017-12-13 17:50:17 -0500

    buttons fixed, thanks Mikael

--- src/main/java/dinndr/DinndrController.java
+++ src/main/java/dinndr/DinndrController.java
@@ -8,20 +8,20 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class DinndrController {
-	
+
 	@Resource
 	private DishRepo dishRepo;
-	
-//	@RequestMapping("/dish")
-//	public String fetchDish(@RequestParam("id") long id, Model model) {
-//		model.addAttribute("dish", dishRepo.findOne(id));
-//		return "dish";
-//	}
+
+	// @RequestMapping("/dish")
+	// public String fetchDish(@RequestParam("id") long id, Model model) {
+	// model.addAttribute("dish", dishRepo.findOne(id));
+	// return "dish";
+	// }
 
 	@RequestMapping("/dishes")
 	public String fetchDishes(Model model) {
 		model.addAttribute("dishes", dishRepo.findAll());
 		return "dishes";
 	}
-	
+
 }
--- src/main/java/dinndr/DishRepo.java
+++ src/main/java/dinndr/DishRepo.java
@@ -5,5 +5,5 @@ import org.springframework.stereotype.Component;
 
 @Component
 public interface DishRepo extends CrudRepository<Dish, Long> {
-	
+
 }
