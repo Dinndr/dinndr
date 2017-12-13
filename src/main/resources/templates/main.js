22078735baa897e281c05363cdc72522e923271c
Todd Atkins <ToddAtkins01@users.noreply.github.com>
2017-12-12 19:30:34 -0500

    statred view 1 image
    statred view 1 image

--- src/main/resources/templates/main.js
+++ src/main/resources/templates/main.js
@@ -1,7 +1,9 @@
 function viewMore() {
-	document.getElementById("viewMoreButton").addEventListener("click", function(){
-		  document.getElementById("information").innerHTML = "To order visit their website.";
-		});
+	document
+			.getElementById("viewMoreButton")
+			.addEventListener(
+					"click",
+					function() {
+						document.getElementById("information").innerHTML = "To order visit their website.";
+					});
 }
-
-
