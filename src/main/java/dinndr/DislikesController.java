7023bce7e14aba28bf5260fcd12606a0299ea607
Michael Smith <mjsmitths@gmail.com>
2017-12-20 20:36:21 -0500

    controller edited

--- src/main/java/dinndr/DislikesController.java
+++ src/main/java/dinndr/DislikesController.java
@@ -1,9 +1,5 @@
 package dinndr;
 
-
-import java.util.ArrayList;
-import java.util.Collection;
-
 import javax.annotation.Resource;
 
 import org.apache.commons.logging.Log;
@@ -15,30 +11,24 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class DislikesController {
-	
-	@Resource 
+
+	@Resource
 	private UserProfileRepo userProfileRepo;
-	
+
 	@Resource
 	private DishRepo dishRepo;
 
-	
-	
-	
-	Collection<Dish> liked = new ArrayList<>();
-
-	Collection<Dish> disliked = new ArrayList<>();	
-	
 	private Log log = LogFactory.getLog(getClass());
-	@RequestMapping(value="/dishes/{id}/disliked", method = RequestMethod.PUT)
-    public void assignDisliked(@PathVariable Long id){
-		
+
+	@RequestMapping(value = "/dishes/{id}/disliked", method = RequestMethod.PUT)
+	public void assignDisliked(@PathVariable Long id) {
+
 		UserProfile theOne = userProfileRepo.findOne(1L);
 		Dish dislikedDish = dishRepo.findOne(id);
-		disliked.add(dislikedDish);
+		theOne.getDisliked().add(dislikedDish);
 		theOne = userProfileRepo.save(theOne);
-		
+		log.info(theOne.getDisliked());
 		log.info("fuu");
-    }
+	}
 
 }
