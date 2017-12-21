bd198c70bb7ee61f483243e1a99671c26daa09a7
programmerjee <angelarowe7@gmail.com>
2017-12-21 18:45:25 -0500

    initial commit

--- src/main/resources/scripts/main.js
+++ src/main/resources/scripts/main.js
@@ -15,6 +15,32 @@ function nextPlease() {
 		x.style.display = "none";
 	}
 };
+function myMap() {
+	var info = document.querySelector(".info");
 
+	var myLat = parseFloat(info.dataset.latitude);
+	var myLng = parseFloat(info.dataset.longitude);
+
+	var myCenter = new google.maps.LatLng(myLat, myLng);
+	var myRestaurant = {
+		center : myCenter,
+		zoom : 15,
+	};
+	var map = new google.maps.Map(document.getElementById("map"),
+			myRestaurant);
+	var marker = new google.maps.Marker({
+		position : myCenter
+	});
+	marker.setMap(map);
+}; 
+//find current dish id
+//default is get to pull browser, put to update
+$.ajax({
+    url: 'https://localhost:8080/dishes/{id}/dislike',
+    // method should be http PUT
+     
+    });
+//.done and call function
+//access object elements
 
 	
