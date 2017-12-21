f29155fa77323186fe3084900fcd31e9929ed138
Michael Smith <mjsmitths@gmail.com>
2017-12-21 17:59:06 -0500

    Liked

---
+++ src/main/java/dinndr/LikedController.java
@@ -0,0 +1,35 @@
+package dinndr;
+
+import javax.annotation.Resource;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class LikedController {
+	
+	@Resource
+	private UserProfileRepo userProfileRepo;
+
+	@Resource
+	private DishRepo dishRepo;
+
+	private Log log = LogFactory.getLog(getClass());
+
+	@RequestMapping(value = "/dishes/{id}/liked", method = RequestMethod.PUT)
+	public void assignLiked(@PathVariable Long id) {
+
+		UserProfile theOne = userProfileRepo.findOne(1L);
+		Dish likedDish = dishRepo.findOne(id);
+		theOne.getLiked().add(likedDish);
+		theOne = userProfileRepo.save(theOne);
+		log.info(theOne.getLiked());
+		log.info("bar");
+	}
+
+
+}
